0f3a172e951da2d90028161e6e6c97d6536d13cc
Hugo Shi <v378073@haldev.svr.us.jpmchase.net>
2013-09-28 11:00:34 -0400

    update

--- arraymanagement/config.py
+++ arraymanagement/config.py
@@ -2,7 +2,12 @@ import json
 import os
 from os.path import join, dirname, exists
 
+import databag
+
+from pathutils import recursive_config_load
+
 class NodeConfig(object):
+    is_dataset = False
     csv_exts = ['csv', 'CSV']
     json_exts = ['json', 'JSON']
     hdf5_exts = ['hdf5', 'h5']
@@ -10,9 +15,19 @@ class NodeConfig(object):
     json_type = 'pandas'
     csv_reader = 'pandas'
     array_cache = '__init__.hdf5'
-    def __init__(self, path, existing_config={}):
-        cfile = join(path, 'config.json')
-        if exists(cfile):
-            
+
+    def __init__(self, path, config):
+        self.config = config
+        self.path = path
+        self.md = databag.DataBag(join(path, "__md.db"))
+
+    @classmethod
+    def from_paths(cls, path, basepath):
+        config = recursive_js_load(path, basepath)
+        return cls(path, existing_config=config)
+
+    def get(self, key):
+        return self.config.get(key)
+
         
         
