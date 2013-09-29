6e8d8001f6fd6eadde228f60204c92fd0d8e6687
Hugo Shi <v378073@haldev.svr.us.jpmchase.net>
2013-09-28 20:50:14 -0400

    forgot files

--- arraymanagement/client.py
+++ arraymanagement/client.py
@@ -5,7 +5,7 @@ from os.path import join, dirname
 class ArrayClient(object):
     def __init__(self, path):
         self.root = path
-        self.md = databag.DataBag(join(sel.root, "__md.db"))
+        self.md = databag.DataBag(join(self.root, "__md.db"))
     
         
 
