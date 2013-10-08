ca4772f89383fc9f4524f335ba9ed8e7df2b08fb
Hugo Shi <v378073@haldev.svr.us.jpmchase.net>
2013-10-08 11:01:48 -0400

    making the client interface nicer (more node like

--- arraymanagement/client.py
+++ arraymanagement/client.py
@@ -4,17 +4,22 @@ from os.path import join, dirname, isdir, relpath, exists, abspath
 from config import NodeConfig
 import pathutils
 from nodes.dirnodes import DirectoryNode
+from nodes import Node
 import default_loader
 import sys
 
-class ArrayClient(object):
+class ArrayClient(Node):
+    #should modify this to inherit from DirectorNode
     def __init__(self, path):
         self.root = path
         self.config = NodeConfig.from_paths(self.root, self.root, self)
         if self.root not in sys.path:
             sys.path.append(self.root)
+        super(ArrayClient, self).__init__("/", ".", self.root, self.config)
 
     def get_node(self, urlpath):
+        if not urlpath.startswith("/"):
+            urlpath = "/" + urlpath
         names = pathutils.urlsplit(urlpath, "/")
         basepath = self.root
         rpath = relpath(basepath, basepath)
@@ -23,8 +28,10 @@ class ArrayClient(object):
         for n in names:
             node = node.get_node(n)
         return node
-            
-            
+
+    def keys(self):
+        return self.get_node('/').keys()
             
         
 
+    
