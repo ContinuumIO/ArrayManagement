c95b03bac3ea295543fc2a070866c223baca6e47
Hugo Shi <v378073@cauchy.svr.us.jpmchase.net>
2013-10-31 13:23:44 -0400

    removed extra imports

--- example/custom2/load.py
+++ example/custom2/load.py
@@ -1,10 +1,5 @@
-import posixpath
-from os.path import join, relpath
 import pandas as pd
-
-from arraymanagement import default_loader
 from arraymanagement.nodes.hdfnodes import PandasCacheableTable
-from arraymanagement.nodes import NodeContext
 
 def get_factor_data(self):
     path = self.joinpath('sample.csv')
