7bd4e5b33a2b303cbe19aaec230317953a1aa38b
Hugo Shi <v378073@cauchy.svr.us.jpmchase.net>
2013-11-19 17:21:30 -0500

    added pytables file closing to clear_mem_cache

--- arraymanagement/__init__.py
+++ arraymanagement/__init__.py
@@ -1,5 +1,7 @@
 import nodes.hdfnodes as hdfnodes
+import tables.file
+
 def clear_mem_cache():
     hdfnodes.pandas_hdf5_cache.clear()
     hdfnodes.PandasCacheableFixed.inmemory_cache.clear()
-
+    tables.file.close_open_files()
