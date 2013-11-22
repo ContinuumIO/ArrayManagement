e2fcd52e3e33f6d3369be0ad6a6008c4964d5636
Hugo Shi <v378073@cauchy.svr.us.jpmchase.net>
2013-11-22 11:04:57 -0500

    function for closing all pytables files without printing

--- arraymanagement/__init__.py
+++ arraymanagement/__init__.py
@@ -4,4 +4,12 @@ import tables.file
 def clear_mem_cache():
     hdfnodes.pandas_hdf5_cache.clear()
     hdfnodes.PandasCacheableFixed.inmemory_cache.clear()
-    tables.file.close_open_files()
+    close_pytables()
+
+def close_pytables():
+    _open_files = tables.file._open_files
+    are_open_files = len(_open_files) > 0
+    for fname, fileh in _open_files.items():
+        fileh.close()
+
+
