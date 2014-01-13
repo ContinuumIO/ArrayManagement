66c598b2e9c99cfedaf843d045f44b8cf53d3eff
hugo <hugo>
2014-01-13 17:11:07 +0800

    data writing/reading has to be done inside context manager

--- arraymanagement/nodes/sql.py
+++ arraymanagement/nodes/sql.py
@@ -63,42 +63,36 @@ class SimpleQueryTable(PandasCacheableTable):
         with self.db() as db:
             return sql.read_frame(query, db)
     
-    def execute_query(self, query=None):
-        if query is None:
-            query = self.query
-        with self.db() as db:
-            cur = db.cursor()
-            cur.execute(self.query)
-            return cur
-
     def load_data(self):
         store = self.store
-        logger.debug("query executing!")
-        cur = self.execute_query()
-        logger.debug("query returned!")
-        logger.debug("cursor descr %s", cur.description)
-        
-        min_itemsize = self.min_itemsize if self.min_itemsize else {}
-        db_string_types = self.db_string_types if self.db_string_types else []
-        db_datetime_types = self.db_datetime_types if self.db_datetime_types else []
-        
-        columns, min_itemsize, dt_fields = query_info(
-            cur,
-            min_itemsize=min_itemsize,
-            db_string_types=db_string_types,
-            db_datetime_types=db_datetime_types
-            )
-        self.min_itemsize = min_itemsize
-        logger.debug("queryinfo %s", str((columns, min_itemsize, dt_fields)))
-        overrides = self.col_types
-        overrides = self.col_types
-        for k in dt_fields:
-            overrides[k] = 'datetime64[ns]'
-        write_pandas_hdf_from_cursor(self.store, self.localpath, cur, 
-                                     columns, self.min_itemsize, 
-                                     dtype_overrides=overrides,
-                                     min_item_padding=self.min_item_padding,
-                                     chunksize=50000, 
-                                     replace=True)
-        cur.close()
-        self.store.flush()
+        with self.db() as db:
+            logger.debug("connected db!")
+            cur = db.cursor()
+            logger.debug("query executing!")
+            cur.execute(self.query)
+            logger.debug("query returned!")
+            logger.debug("cursor descr %s", cur.description)
+
+            min_itemsize = self.min_itemsize if self.min_itemsize else {}
+            db_string_types = self.db_string_types if self.db_string_types else []
+            db_datetime_types = self.db_datetime_types if self.db_datetime_types else []
+
+            columns, min_itemsize, dt_fields = query_info(
+                cur,
+                min_itemsize=min_itemsize,
+                db_string_types=db_string_types,
+                db_datetime_types=db_datetime_types
+                )
+            self.min_itemsize = min_itemsize
+            logger.debug("queryinfo %s", str((columns, min_itemsize, dt_fields)))
+            overrides = self.col_types
+            for k in dt_fields:
+                overrides[k] = 'datetime64[ns]'
+            write_pandas_hdf_from_cursor(self.store, self.localpath, cur, 
+                                         columns, self.min_itemsize, 
+                                         dtype_overrides=overrides,
+                                         min_item_padding=self.min_item_padding,
+                                         chunksize=50000, 
+                                         replace=True)
+            cur.close()
+            self.store.flush()
