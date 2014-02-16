108a7bfe717c55cf1e324075965c8745dbb7bc99
hugo <hugo>
2014-02-16 14:26:49 -0500

    removing setuptools for develop

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 # needs to be tested
+from __future__ import print_function
 import sys
 if len(sys.argv)>1 and sys.argv[1] == 'develop':
     # Only import setuptools if we have to
-    import setuptools
+    import site
+    from os.path import dirname, abspath, join
+    site_packages = site.getsitepackages()[0]
+    fname = join(site_packages, "arraymanagement.pth")
+    path = abspath(dirname(__file__))
+    with open(fname, "w+") as f:
+        f.write(path)
+    print("develop mode, wrote path (%s) to (%s)" % (path, fname))
+    sys.exit()
 from distutils.core import setup
 import os
 import sys
